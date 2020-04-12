af9397b293cefc31f878232649ee2eae194af8d2
ankitgupta123445 <ankit.gla_cs18@gla.ac.in>
2020-04-13 03:10:24 +0530

    search() method in Binarysearch class is complete

--- src/BinarySearch/BinarySearch.java
+++ src/BinarySearch/BinarySearch.java
@@ -1,4 +1,27 @@
 package BinarySearch;
+import Exception.NumberNotFoundException;
+
+import java.util.Arrays;
 
 public class BinarySearch {
+        public int search(int[] array, int number) throws NumberNotFoundException {
+                Arrays.sort(array);
+                int response = -1;
+                int startIndex = 0;
+                int endIndex = array.length - 1;
+                while (startIndex <= endIndex) {
+                        int mid = (startIndex + endIndex) / 2;
+                        if (array[mid] == number) {
+                                response = mid;
+                                break;
+                        }
+                        if (array[mid] < number) {
+                                startIndex = mid + 1;
+                        }
+                        if (array[mid] > number) {
+                                endIndex = mid - 1;
+                        }
+                }
+                return response;
+        }
 }
